8248f6141220cff215048c202d6f8b61b2f1d8a4
Kurt Gilpin <kurt@gilpin.info>
2021-06-26 19:10:18 -0500

    Updated to use ->Message instead of DiaWnd

--- plugins/SpellsChecker.pl
+++ plugins/SpellsChecker.pl
@@ -1,76 +1,39 @@
 sub ShowSpellList {
-  #my $character_classInsert = $_[0];
   my $class = $_[0];
-  #my $OrderBy = $_[2];
-  #my $QueryLimit = 10;
-	
-  $connect = plugin::LoadMysql();
+  my $levelrange = $_[1];
+  my $client = plugin::val('$client');
 
-  $query = "SELECT
-    `Name`,
-    `class`,
-    `level`,
-    `expansion`,
-    `spell_id`
-      FROM
-    `va_spells`
-      WHERE class = ? ORDER BY level";
-  $query_handle = $connect->prepare($query);
-
-  ###  $query_handle->execute($class);
-  $query_handle->execute(
-    $class
-  );
-  quest::debug("$DBI::errstr") if $DBI::errstr;
-  my $Result = "";
-  while (@row = $query_handle->fetchrow_array()){
-  #my ($spell_name, $spell_class, $spell_level, $spell_expansion, $spell_id) = $query_handle->fetchrow();
-  #quest::debug("Name: $spell_name");
-  #quest::debug("Class $spell_class");
-  #quest::debug("Level $spell_level");
-  #quest::debug("Expansion $spell_expansion");
-  #quest::debug("ID $spell_id");
-
-  #  my $Result = "";
-  #while (@row = $query_handle->fetchrow_array()){
-    my $spell_name = $row[0];
-    my $spell_class = $row[1];
-    my $spell_level = $row[2];
-    my $spell_expansion = $row[3];
-    my $spell_id = $row[4];
-    quest::debug("ID $spell_id");
-    $count++;
-	
-  $Result .= "
-    <tr>
-      <td><c \"#FFFF66\">$count~</td>
-      <td><c \"#FFFF66\">" . $spell_name . "~</td>
-      <td><c \"#DF7401\">" . $spell_class . "~ </td>
-      <td><c \"#00FF00\">" . $spell_level . "~ </td>
-      <td><c \"#FF0000\">" . $spell_expansion . "~ </td>
-      <td><c \"#3366FF\">" . $spell_id . "~ </td>
-    </tr>";
+  if($levelrange eq "low"){
+    $level_low = 1;
+    $level_high = 20;
+  } elsif($levelrange eq "medium"){
+    $level_low = 21;
+    $level_high = 40;
+  } elsif($levelrange eq "high"){
+    $level_low = 41;
+    $level_high = 60;
+  } elsif($levelrange eq "max"){
+    $level_low = 61;
+    $level_high = 65;
+  } elsif($levelrange eq "all"){
+    $level_low = 1;
+    $level_high = 65;
   }
 
-  $count = 0;
-  plugin::DiaWind(
-    "
-    {linebreak}
-    <br>
-    Spells ~
-    <br>
-    {linebreak}
-    <br>
-    <table>
-      <tr>
-        <th></th>
-        <th>Name</th>
-        <th>Class</th>
-        <th>Level</th>
-        <th>Expansion</th>
-        <th>Spell ID</th>
-      </tr>
-      " . $Result . "
-    </table>
-  ");
+  $connect = plugin::LoadMysql();
+  $query = "SELECT
+    `Name`,
+    `level`,
+    `expansion`
+      FROM
+    `va_spells2`
+      WHERE class = ? AND level >= ? and level <= ? ORDER BY level ASC";
+
+  $query_handle = $connect->prepare($query);
+  $query_handle->execute($class,$level_low,$level_high);
+
+  while (@row = $query_handle->fetchrow_array()){
+    my ($spell_name, $spell_level, $spell_expansion) = $query_handle->fetchrow();
+    $client->Message(315, $spell_name . " - " . $spell_level . " - " . $spell_expansion);
+  }
 }
